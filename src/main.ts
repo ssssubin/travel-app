cb859e72e3a7003dcf66497d1947b17e7ebfe452
subin <cdw3521@naver.com>
2024-11-14 17:50:22 +0900

    🔧 CONFIG - 전역 prefix 설정 - 예외처리 필터 -> 전역 필터로 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { httpExceptionFilter } from "./filter/http-exception.filter";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+   const app = await NestFactory.create(AppModule);
+   app.setGlobalPrefix("/api"); // 모든 경로에 /api 붙임
+   app.useGlobalFilters(new httpExceptionFilter()); // 예외처리 필터 -> 전역 필터
+
+   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
