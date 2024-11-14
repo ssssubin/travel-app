76cfed4b08a55e42f79a5caa7918409b7c1bf515
subin <cdw3521@naver.com>
2024-11-14 17:49:41 +0900

    🔧 CONFIG: 환경변수 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+   imports: [
+      ConfigModule.forRoot({
+         envFilePath: ".env",
+         isGlobal: true, // 전역 모듈로 선언
+      }),
+   ],
+   controllers: [AppController],
+   providers: [AppService],
 })
 export class AppModule {}
