f887f9540af3c0dd05c172aaa490b6bf2c63ed66
subin <cdw3521@naver.com>
2024-11-15 15:53:18 +0900

    ✨ import 추가

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AccountController } from "./account.controller";
 import { AccountService } from "./account.service";
 import { DataModule } from "src/data/data.module";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
-   imports: [DataModule],
+   imports: [DataModule, JwtModule],
    controllers: [AccountController],
    providers: [AccountService],
 })
