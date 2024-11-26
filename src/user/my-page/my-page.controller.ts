2ec8fb14425aad0f554b13769ba51585fc974511
ssssu <cdw3521@naver.com>
2024-11-26 21:22:08 +0900

    ✨ FEAT: 마이페이지 조회 API 구현

--- src/user/my-page/my-page.controller.ts
+++ src/user/my-page/my-page.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get, Req, Res } from "@nestjs/common";
+import { Controller, Get, Res } from "@nestjs/common";
 import { MyPageService } from "./my-page.service";
-import { Request, Response } from "express";
+import { Response } from "express";
 
 @Controller("my-page")
 export class MyPageController {
    constructor(private mypageService: MyPageService) {}
    @Get()
-   async getMyPage(@Res({ passthrough: true }) res: Response, @Req() req: Request) {
-      return await this.mypageService.getMyPage(res, req);
+   async getMyPage(@Res({ passthrough: true }) res: Response) {
+      return await this.mypageService.getMyPage(res);
    }
 }
