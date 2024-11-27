bdfe23a8d658a452602e6410b8847cce644faa59
ssssu <cdw3521@naver.com>
2024-11-27 21:17:15 +0900

    ✨ FEAT: 프로필 조회 API 구현

--- src/user/my-page/my-page.controller.ts
+++ src/user/my-page/my-page.controller.ts
@@ -5,8 +5,14 @@ import { Response } from "express";
 @Controller("my-page")
 export class MyPageController {
    constructor(private mypageService: MyPageService) {}
+
    @Get()
    async getMyPage(@Res({ passthrough: true }) res: Response) {
       return await this.mypageService.getMyPage(res);
+   }
+
+   @Get("profile")
+   async getProfile(@Res({ passthrough: true }) res: Response) {
+      return await this.mypageService.getProfile(res);
    }
 }
