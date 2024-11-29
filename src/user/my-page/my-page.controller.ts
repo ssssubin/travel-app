9634695233d43c960d44cf669fa21c56256e0ae0
subin <cdw3521@naver.com>
2024-11-29 11:33:44 +0900

    ✨ FEAT: 프로필 수정 API 구현

--- src/user/my-page/my-page.controller.ts
+++ src/user/my-page/my-page.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Put, Res } from "@nestjs/common";
 import { MyPageService } from "./my-page.service";
 import { Response } from "express";
+import { updateUserDto } from "../dto/update-user.dto";
 
 @Controller("my-page")
 export class MyPageController {
@@ -16,10 +17,16 @@ export class MyPageController {
       return await this.mypageService.getProfile(res);
    }
 
+   @Put("profile")
+   async updateProfile(@Res({ passthrough: true }) res: Response, @Body() updateData: updateUserDto) {
+      return await this.mypageService.updateProfile(res, updateData);
+   }
+
    @Get("keyword")
    async getKeyword() {
       return await this.mypageService.getKeyword();
    }
+
    @Put("keyword")
    async updateKeyword(@Res({ passthrough: true }) res: Response, @Body() data: { keyword: string[] }) {
       return await this.mypageService.updateKeyword(res, data.keyword.slice(0, 5));
