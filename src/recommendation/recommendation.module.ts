c180b292e718432e4039511e9c477746c6165f73
ssssu <cdw3521@naver.com>
2024-12-02 00:20:01 +0900

    ✨ import 추가

--- src/recommendation/recommendation.module.ts
+++ src/recommendation/recommendation.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { DataModule } from "src/data/data.module";
 import { RecommendationController } from "./recommendation.controller";
 import { RecommendationService } from "./recommendation.service";
+import { UserModule } from "src/user/user.module";
 
 @Module({
-   imports: [DataModule],
+   imports: [DataModule, UserModule],
    controllers: [RecommendationController],
    providers: [RecommendationService],
 })
