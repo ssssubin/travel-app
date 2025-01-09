f8ff240f89f0095186b8f627c2d41fb230731bf7
ssssu <cdw3521@naver.com>
2025-01-09 22:29:54 +0900

    ✅ e2e 테스트 코드 작성(for 회원가입/로그인 API)

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,87 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import { AppModule } from "@src/app.module";
+import { createUserDto, signInUserDto } from "@user/dto/user.dto";
+import { ConfigModule } from "@nestjs/config";
+import { v4 } from "uuid";
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe("AppController (e2e)", () => {
+   let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+   const signUpUser: createUserDto = {
+      email: `${v4()}@test.com`,
+      name: "신짱구",
+      password: "test1234!",
+      confirmPassword: "test1234!",
+      continent: "아시아",
+      country: "대한민국",
+      city: "서울",
+   };
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+   const signInUser: signInUserDto = {
+      email: "test@test.com",
+      password: "test1234!",
+   };
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
-  });
+   // 테스트 진행하기 전 가장 먼저 실행되는 함수
+   beforeEach(async () => {
+      const moduleFixture: TestingModule = await Test.createTestingModule({
+         imports: [
+            ConfigModule.forRoot({
+               // test용 db와 연결
+               envFilePath: ".test.env",
+               isGlobal: true,
+            }),
+            AppModule,
+         ],
+      }).compile();
+
+      app = moduleFixture.createNestApplication();
+      await app.init();
+   });
+
+   describe("회원가입/로그인 API", () => {
+      it("/sign-up (POST)", async () => {
+         const res = await request(app.getHttpServer()).post("/sign-up").send(signUpUser);
+         expect(res.status).toBe(201);
+         expect(res.body.data).toStrictEqual("회원가입 되었습니다. 로그인 해주세요 :)");
+      });
+
+      it("/sign-up/continent (GET)", async () => {
+         const res = await request(app.getHttpServer()).get("/sign-up/continent");
+
+         expect(res.status).toBe(200);
+         expect(res.body.data).toStrictEqual([
+            "아시아",
+            "유럽",
+            "북아메리카",
+            "남아메리카",
+            "아프리카",
+            "오세아니아",
+            "남극",
+         ]);
+      });
+
+      it("/sign-up/country (POST)", async () => {
+         const res = await request(app.getHttpServer()).post("/sign-up/country").send({ continent: "아시아" });
+
+         expect(res.status).toBe(200);
+         expect(res.body.data).toStrictEqual(["대한민국", "일본", "중국"]);
+      });
+
+      it("/sign-up/city (POST)", async () => {
+         const res = await request(app.getHttpServer()).post("/sign-up/city").send({ country: "대한민국" });
+
+         expect(res.status).toBe(200);
+         expect(res.body.data).toStrictEqual(["서울", "인천", "광주"]);
+      });
+
+      it("/sign-in (POST)", async () => {
+         const res = await request(app.getHttpServer()).post("/sign-in").send(signInUser);
+
+         expect(res.status).toBe(200);
+         expect(res.body.data).toStrictEqual("로그인에 성공하셨습니다. 환영합니다 :)");
+      });
+   });
 });
