8e24b3540f2ba4b2e15468a48fa0d3c33e69d5f5
ssssu <cdw3521@naver.com>
2025-01-20 10:33:04 +0900

    ☁️이미지 저장소 변경에 따른 코드 수정 - 이미지 저장소: DISK -> AWS S3

--- src/user/my-page/my-page.controller.ts
+++ src/user/my-page/my-page.controller.ts
@@ -16,8 +16,6 @@ import { Response } from "express";
 import { updateUserDto } from "@user/dto/update-user.dto";
 import { createReviewDto } from "@mypage/dto/create-review.dto";
 import { FilesInterceptor } from "@nestjs/platform-express";
-import { diskStorage } from "multer";
-import { extname } from "path";
 import { ReviewService } from "@mypage/services/my-page-review.service";
 
 @Controller("my-page")
@@ -58,66 +56,20 @@ export class MyPageController {
    }
 
    @Post("review")
-   @UseInterceptors(
-      FilesInterceptor("images", null, {
-         storage: diskStorage({
-            destination: "./uploads/temp", // 임시로 저장할 디렉토리
-            filename: (req, file, callback) => {
-               const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-               const ext = extname(file.originalname);
-               callback(null, `image-${uniqueSuffix}${ext}`);
-            },
-         }),
-         fileFilter: (req, file, callback) => {
-            if (!file) {
-               // null 허용하기 위해 필터 통과
-               return callback(null, true);
-            }
-            const allowedMimeTypes = ["image/jpeg", "image/png", "image/jpg"];
-            if (allowedMimeTypes.includes(file.mimetype)) {
-               callback(null, true);
-            } else {
-               callback(new Error("잘못된 파일입니다."), false);
-            }
-         },
-      }),
-   )
+   @UseInterceptors(FilesInterceptor("image"))
    async createReview(
       @Res({ passthrough: true }) res: Response,
-      @UploadedFiles() images: Express.Multer.File | null,
+      @UploadedFiles() images: Express.Multer.File[],
       @Body() data: createReviewDto,
    ) {
       return await this.reviewService.createReview(res, data, images);
    }
 
    @Put("review/:id")
-   @UseInterceptors(
-      FilesInterceptor("images", null, {
-         storage: diskStorage({
-            destination: "./uploads/temp", // 임시로 저장할 디렉토리
-            filename: (req, file, callback) => {
-               const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-               const ext = extname(file.originalname);
-               callback(null, `image-${uniqueSuffix}${ext}`);
-            },
-         }),
-         fileFilter: (req, file, callback) => {
-            if (!file) {
-               // null 허용하기 위해 필터 통과
-               return callback(null, true);
-            }
-            const allowedMimeTypes = ["image/jpeg", "image/png", "image/jpg"];
-            if (allowedMimeTypes.includes(file.mimetype)) {
-               callback(null, true);
-            } else {
-               callback(new Error("잘못된 파일입니다."), false);
-            }
-         },
-      }),
-   )
+   @UseInterceptors(FilesInterceptor("image"))
    async updateReview(
       @Res({ passthrough: true }) res: Response,
-      @UploadedFiles() images: Express.Multer.File | null,
+      @UploadedFiles() images: Express.Multer.File[],
       @Param("id") id: string,
       @Body() data: createReviewDto,
    ) {
